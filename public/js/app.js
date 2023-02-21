ab943fa5ac6a1347f80e9de48ef8806d9e0dda7f
Billy Franscois <BillyFranscois@outlook.com>
2023-02-22 00:46:33 +0900

    Update webpack

--- public/js/app.js
+++ public/js/app.js
@@ -151,7 +151,7 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkbookdatasystem"] = self["webpackChunkbookdatasystem"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkbookmanagementsystem"] = self["webpackChunkbookmanagementsystem"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
